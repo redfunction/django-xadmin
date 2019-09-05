bb6a669d0b65e17130ae52ac0e9a3a2853213817
alexsilva <geniofuturo@gmail.com>
2019-09-05 12:16:38 -0300

    Update datepicker api.

--- xadmin/static/xadmin/js/xadmin.plugin.filters.js
+++ xadmin/static/xadmin/js/xadmin.plugin.filters.js
@@ -30,25 +30,21 @@
       var end_date = el.find('.calendar.date-end').datepicker({format: 'yyyy-mm-dd', language: 'xadmin'});
       
       var checkAvailable = function(){
-        var start_date_dt = start_date.data('datepicker').getDate();
-        var end_date_dt = end_date.data('datepicker').getDate();
-        if(start_date_dt && end_date_dt && start_date_dt.valueOf() <= end_date_dt.valueOf()){
-          el.find('button[type=submit]').removeAttr('disabled');
-        } else {
-          el.find('button[type=submit]').attr('disabled', 'disabled');
-        }
+        var start_date_dt = start_date.datepicker('getDate');
+        var end_date_dt = end_date.datepicker('getDate');
+        el.find('button[type=submit]').prop('disabled', start_date_dt >= end_date_dt);
       }
       
       start_date.on('changeDate', function(ev){
-          var startdate = start_date.data('date');
+          var startdate = start_date.datepicker('getFormattedDate');
           el.find('.start_input').val(startdate);
-          end_date.data('datepicker').setStartDate(startdate);
+          end_date.datepicker('setStartDate', startdate);
           checkAvailable();
       });
       end_date.on('changeDate', function(ev){
-          var enddate = end_date.data('date');
+          var enddate = end_date.datepicker('getFormattedDate');
           el.find('.end_input').val(enddate);
-          start_date.data('datepicker').setEndDate(enddate);
+          start_date.datepicker('setEndDate', enddate);
           checkAvailable();
       });
       
