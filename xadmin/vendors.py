0b9f5879b73d0397c6fbd8210f7f8c7ed89d40bf
alex <geniofuturo@gmail.com>
2020-04-04 20:57:47 -0300

    Register html5sortable vendor.

--- xadmin/vendors.py
+++ xadmin/vendors.py
@@ -52,14 +52,10 @@ vendors = {
     'jquery-ui-sortable': {
         "js": {
             'dev': [
-                'xadmin/vendor/jquery-ui/ui/widget.js',
-                'xadmin/vendor/jquery-ui/ui/widgets/mouse.js',
-                'xadmin/vendor/jquery-ui/ui/widgets/sortable.js'
+                'xadmin/vendor/html5sortable/html5sortable.js',
             ],
             'production': [
-                'xadmin/vendor/jquery-ui/ui/minified/widget.js',
-                'xadmin/vendor/jquery-ui/ui/minified/mouse.js',
-                'xadmin/vendor/jquery-ui/ui/widgets/sortable.js'
+                'xadmin/vendor/html5sortable/html5sortable.min.js'
             ]
         }
     },
