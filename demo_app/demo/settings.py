6311186634968a1c78982339ffa4d8a0759c44d6
alex <geniofuturo@gmail.com>
2020-11-16 11:35:08 -0300

    New db; new migrations..

--- demo_app/demo/settings.py
+++ demo_app/demo/settings.py
@@ -117,7 +117,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db3.sqlite3'),
     }
 }
 
